60792c0527e36938ac0282f25ca027b88de71616
allenbo <linjianfengqrh@gmail.com>
2013-11-12 19:13:42 +0000

    P fine tune the weight

--- dating/pplayer.py
+++ dating/pplayer.py
@@ -46,84 +46,6 @@ host = '127.0.0.1'
 teamname = 'illuminati-i'
 sock = MySocket(port, host)
 
-'''
-has_to_pos = None
-has_to_neg = None
-could_be_pos = None
-could_be_neg = None
-glr = None
-
-class Candidate(object):
-    def __init__(self, feature, score):
-        self.feature = np.array(feature).astype(np.float32)
-        self.score = score
-
-cans = []
-
-def linear_initialize(cans):
-  X = []
-  Y = []
-  for can in cans:
-    X.append(can.feature.tolist())
-    Y.append(can.score)
-  clf = linear_model.BayesianRidge()
-  clf.fit(X, Y)
-  return clf.coef_
-
-
-def train(cans, learning_rate, weight, epoch = 10000, plot = False):
-  num = len(cans[0].feature)
-  #weight = np.random.randn(num).astype(np.float32)
-  weight[weight>0] /= weight[weight>0].sum()
-  weight[weight<0] /= -1.0 * weight[weight<0].sum()
-
-  absgrads = []
-  for i in range(epoch):
-    if len(absgrads) > 50:
-      last = np.array(absgrads[-11:])
-      prev = np.array(absgrads[-21:-11])
-      if last.mean() > prev.mean():
-        break
-    for can in cans:
-      grad = np.zeros_like(weight)
-      input = can.feature
-      score = can.score
-      yc = (input * weight).sum()
-      grad += grad * (yc - score)
-      absgrads.append(abs(grad))
-      weight = weight - learning_rate * grad
-    weight[weight>0] /= weight[weight>0].sum()
-    weight[weight<0] /= -1.0 * weight[weight<0].sum()
-
-  return weight
-
-def test(weight, can):
-  return abs(can.score - (can.feature * weight).sum())
-
-def sgd(cans, weight):
-  print 'weight before training'
-  print weight
-  global glr
-  train_set = cans[:16]
-  test_set = cans[16:]
-
-  old_weight = weight
-  if glr is None:
-    best = (None, 1000000, 0.0)
-    for lr in [0.00001, 0.0001, 0.001, 0.01, 0.1]:
-      weight = train(cans, lr, weight, epoch = 1000)
-      cost = 0
-      for can in test_set:
-        cost += test(weight, can)
-      if best[1] > cost:
-        best = [weight, cost, lr]
-    glr = best[2]
-
-  weight = train(cans, glr, old_weight,  plot = True)
-  print 'weight after training'
-  print weight
-  return weight
-'''
 
 def np2str(a):
     rst = ''
@@ -131,7 +53,6 @@ def np2str(a):
         rst +=  ('%.2f' % f) + ' '
     rst = rst[:-1]
     return rst
-
 
 def constraint_weight(weight):
   weight[weight > 0] /= weight[weight>0].sum()
@@ -168,4 +89,15 @@ if __name__ == '__main__':
     sock.write(np2str(weight))
     for i in range(20):
       sock.read()
+      idx2, idx1 = weight.argsort()[-2:]
+      amount = weight[idx2] * 0.2
+      if amount > 0.01:
+        weight[idx1] -= amount
+        weight[idx2] += amount
+      if num_feature >= 80:
+        idx1, idx2 = weight.argsort()[:2]
+        amount = weight[idx1] * 0.2
+        if amount > 0.01:
+          weight[idx1] -= amount
+          weight[idx2] += amount
       sock.write(np2str(weight))
