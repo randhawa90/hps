a40c87047332262a82ab76c5cead170cb23c8a7f
allenbo <linjianfengqrh@gmail.com>
2013-11-12 18:07:28 +0000

    Add P player

--- dating/pplayer.py
+++ dating/pplayer.py
@@ -5,7 +5,7 @@ import random
 from sklearn import linear_model
 #import matplotlib.pyplot as plt
 
-debug = False
+debug = True
 
 class MySocket(object):
     MSGLEN = 8888
@@ -43,10 +43,10 @@ class MySocket(object):
 port = int(sys.argv[1])
 host = '127.0.0.1'
 
-teamname = 'illuminati'
+teamname = 'illuminati-i'
 sock = MySocket(port, host)
 
-
+'''
 has_to_pos = None
 has_to_neg = None
 could_be_pos = None
@@ -123,13 +123,21 @@ def sgd(cans, weight):
   print 'weight after training'
   print weight
   return weight
+'''
 
 def np2str(a):
     rst = ''
     for i in a:
-        rst += str(i) + ' '
+        rst +=  '%.2f' % i + ' '
     rst = rst[:-1]
     return rst
+
+
+def create_weight(num_weight):
+  weight = np.random.randn(num_weight)
+  weight[weight > 0] /= weight[weight>0].sum()
+  weight[weight < 0] /= -1.0*weight[weight<0].sum()
+  return weight
 
 if __name__ == '__main__':
     msg = sock.read()
@@ -138,76 +146,12 @@ if __name__ == '__main__':
 
     msg  = sock.read()
     lines = msg.split('\n')
-    M, features = lines[0].split()
+    P, features = lines[0].split()
 
     num_feature = int(features)
-    if has_to_pos is None:
-      has_to_pos = np.zeros(num_feature).astype(np.int32)
-      has_to_neg = np.zeros(num_feature).astype(np.int32)
-      could_be_pos = np.zeros(num_feature).astype(np.float32)
-      could_be_neg = np.zeros(num_feature).astype(np.float32)
+    weight = create_weight(num_feature)
 
-    num = 20
-    for line in lines[1:]:
-        if line.strip() == '':
-            continue
-        candidate = [float(x) for x in line.split()]
-        score = candidate[-1]
-        cans.append(Candidate(candidate[:-1], score))
-
-    for i in range(num):
-      if weight is None:
-        weight = linear_initialize(cans)
-        weight = sgd(cans, weight)
-        has_to_pos[weight > 2.5 / num_feature] = 1
-        could_be_pos[weight > 0] = 1
-        could_be_pos -= has_to_pos
-        has_to_neg[weight < -2.5 / num_feature] = 1
-        could_be_neg[weight < 0] = 1
-        could_be_neg -= has_to_neg
-
-      rst = np.zeros(num_feature).astype(np.float32)
-
-      index1 = 0
-      index2 = 1
-      if i == num - 1:
-        rst[has_to_pos == 1] = 1
-      else:
-        #for i in range(10000):
-        #  index1 = random.randint(0, num_feature-1)
-        #  index2 = random.randint(0, num_feature-1)
-        #  if index1 == index2:continue
-        #  if (could_be_pos[index1] == 1 or could_be_neg[index1] == 1) and could_be_pos[index2] == 1:
-        #    break
-        tmp_weight = np.zeros_like(weight)
-        tmp_weight[could_be_pos == 1] = weight[could_be_pos == 1]
-        index1 = tmp_weight.argmax()
-        tmp_weight = np.ones_like(weight) * -1000
-        tmp_weight[could_be_neg == 1] = weight[could_be_neg == 1]
-        print tmp_weight
-        index2 = tmp_weight.argmax()
-        rst[index1] = 1
-        rst[index2] = 1
-
-      sock.write(np2str(rst))
-      msg = sock.read()
-      cans = []
-      lines = msg.split('\n')
-      print len(lines[1:])
-      for line in lines[1:]:
-        if line.strip() == '':
-            continue
-        candidate = [float(x) for x in line.split()]
-        score = candidate[-1]
-        cans.append(Candidate(candidate[:-1], score))
-      print index1, index2, 'score is', cans[-1].score
-      if cans[-1].score < 0:
-        has_to_neg[index1] = 1
-        has_to_neg[index1] = 1
-      else:
-        has_to_pos[index2] = 1
-        has_to_pos[index2] = 1
-      could_be_pos[index1] = 0
-      could_be_neg[index1] = 0
-      could_be_pos[index2] = 0
-      could_be_neg[index2] = 0
+    sock.write(np2str(weight))
+    for i in range(20):
+      sock.read()
+      sock.write(np2str(weight))
