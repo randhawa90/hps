3a11fb7da77d4ce055d3443e91bc62add4758807
allenbo <linjianfengqrh@gmail.com>
2013-12-03 11:22:07 -0500

    Fix a bug in io-bound source file

--- multi2sim-4.0/final/io-bound.c
+++ multi2sim-4.0/final/io-bound.c
@@ -7,36 +7,43 @@
 #include <fcntl.h>
 #include <sys/stat.h>
 
+#define MAX_LINE 1024
+
 void read_file(const char* filename) {
   struct stat file_stat;
   int fd = open(filename, O_RDONLY);
   fstat(fd, &file_stat);
   int size = file_stat.st_size;
   int len = 0;
-  
-  char* buffer = (char*) malloc( sizeof(char) * size);
+
+  //char* buffer = (char*) malloc( sizeof(char) * size);
+  char buffer[MAX_LINE];
+  while(len < size) {
+    int st = read(fd, buffer, MAX_LINE);
+    len += st;
+  }
+  /*
   len = read(fd, buffer, size);
   if (len != size ) {
     free(buffer);
     fprintf(stderr, "Problem when reading\n");
     return ;
   }
-  
-  free(buffer);
+  */
 }
 
-void * read(void *filename) {
+void * read_thread(void *filename) {
   struct timeval start, end;
   gettimeofday(&start, NULL);
   read_file((const char* )filename);
   gettimeofday(&end, NULL);
   fprintf(stderr, "The running time of reading %s is %f\n", (const char*) filename, 1.0 * ((end.tv_sec - start.tv_sec)*1000000 + (end.tv_usec - start.tv_usec) ) / 1000000);
-  return NULL;  
+  return NULL;
 }
 
 
 int main() {
-  char* fileanmes[] = {"file1",
+  char* filenames[] = {"file1",
              "file2", 
              "file3",
              "file4"};
@@ -44,7 +51,7 @@ int main() {
   pthread_t thread_id[4];
   int i = 0;
   for(i = 0; i < 4; i ++ ) {
-    pthread_create(&thread_id[i], NULL, read, (void*) filenames[i]);
+    pthread_create(&thread_id[i], NULL, read_thread, (void*) filenames[i]);
   }
 
   for(i = 0; i < 4; i ++ ) {
